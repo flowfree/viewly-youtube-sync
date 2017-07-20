e2444e5c5954f4cdfe5b1eff8cbf0d0b5b0d363f
Nash <nashruddin.amin@gmail.com>
2017-07-20 19:08:30 +0700

    list uploaded videos

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 import json
 
 from flask import (
-    Flask, request, session, url_for, redirect,
+    Flask, request, session, url_for, redirect, jsonify,
 )
 from apiclient import discovery
 from oauth2client import client
@@ -21,18 +21,26 @@ def index():
     credentials = client.OAuth2Credentials.from_json(session['credentials'])
     if credentials.access_token_expired:
         return redirect(url_for('oauth2callback'))
-    else:
-        http_auth = credentials.authorize(httplib2.Http())
-        youtube = discovery.build('youtube', 'v3', http_auth)
-        channel = youtube.channels().list(mine=True, part='snippet').execute()
-        return json.dumps(channel)
+    http_auth = credentials.authorize(httplib2.Http())
+    youtube = discovery.build('youtube', 'v3', http_auth)
+    channel = youtube.channels().list(mine=True, part='contentDetails').execute()
+    try:
+        playlist_id = channel['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+    except KeyError:
+        return 'No uploaded videos.'
+    videos = youtube.playlistItems().list(
+        playlistId=playlist_id, 
+        maxResults=25, 
+        part='snippet,contentDetails',
+    ).execute()
+    return jsonify(videos)
 
 
 @app.route('/oauth2callback')
 def oauth2callback():
     flow = client.flow_from_clientsecrets(
         'client_secrets.json',
-        scope='https://www.googleapis.com/auth/youtube.force-ssl',
+        scope='https://www.googleapis.com/auth/youtube.readonly',
         redirect_uri=url_for('oauth2callback', _external=True),
     )
     if 'code' not in request.args:
@@ -43,6 +51,16 @@ def oauth2callback():
         credentials = flow.step2_exchange(auth_code)
         session['credentials'] = credentials.to_json()
         return redirect(url_for('index'))
+
+
+@app.route('/revoke')
+def revoke():
+    if 'credentials' in session:
+        credentials = client.OAuth2Credentials.from_json(session['credentials'])
+        credentials.revoke(httplib2.Http())
+        return 'access revoked.'
+    else:
+        return 'no credentials in session'
 
 
 if __name__ == "__main__":
